d2fed92dbb83ee9f2138450d559ac2cae4685dfc
Sravaniganthala <146628579+Sravaniganthala@users.noreply.github.com>
2024-11-05 18:07:49 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,76 @@
+// console.log("forloop")
+let i=1
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+console.log(whileloop)//----------whileloop
+var a=0
+while(a<=5){
+    console.log(a)
+    a++
+}
+
+//------------do while
+var j=0
+do{
+    console.log(j)
+    j++
+}
+while(j<=10)
+// console.js("question-2**********even numbers*********")
+
+var m=0
+for(m=0;m<=10;m+=2){
+    console.log(m)
+}
+
+var n=0
+do{
+    console.log(n)
+    n+=2
+}
+while(n<=10)
+    //----------------------odd numbers for loop----------
+let k=1
+for(k=1;k<=10;k+=2){
+    console.log(k)
+}
+//do while loop
+let j=0
+do{
+    console.log(j)
+    j+=2
+}
+while(j<=10)
+//------------------------question-4------------
+//4.write a program to console the marks grade D using conditional statements and the marks for D grade is b/w the 35 and 45 ?
+var marks=40
+if(marks>=65 && marks<=70){
+    console.log("Grad:A");
+}
+else if(marks>=55 && marks<=60){
+    console.log("Grade:B");
+}
+else if(marks>=45 && marks<=50){
+    console.log("Grade:c");
+}
+else if(marks>=35 && marks<=40){
+    console.log("Grade:D");
+}else{
+    console.log("fail")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
